9bb784d20da803ebcdfdee6639365dd5dd46619d
David Tache <tached@uninorte.edu.co>
2023-04-30 20:25:37 -0500

    modified:   src/pedidos/orders.model.js

--- src/pedidos/orders.model.js
+++ src/pedidos/orders.model.js
@@ -3,8 +3,20 @@ const mongoose = require("mongoose");
 const orderSchema = mongoose.Schema(
   {
     restaurantID: { type: String, required: [true] },
-    userID: {type: String, required: [true]},
-    status: { type: String, required: [true] },
+    userID: { type: String, required: [true] },
+    status: {
+      type: String,
+      required: [true],
+      default: "Creado",
+      enum: [
+        "Creado",
+        "Enviado",
+        "Aceptado",
+        "Recibido",
+        "On route",
+        "Realizado",
+      ],
+    },
     isDisable: { type: Boolean, default: false },
   },
   { timestamps: true }
